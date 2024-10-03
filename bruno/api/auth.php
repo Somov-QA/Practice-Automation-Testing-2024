7dbedffa5379470e00fd8fec929125760ca7b39b
Somov Studio <somov.studio@gmail.com>
2024-10-03 13:55:57 +0300

    Практика Bruto

--- bruno/api/auth.php
+++ bruno/api/auth.php
@@ -10,12 +10,23 @@ date_default_timezone_set("Europe/Moscow");
 	http://localhost/api/auth.php?name=admin&pass=0000
 */
 
-if ($_GET["name"] == "admin" && $_GET["pass"] == "0000")
+if (isset($_GET["name"]) && isset($_GET["pass"]))
 {
-	print('{"status":"PASSED","message":"Авторизация прошла успешно"}');
+	if ($_GET["name"] == "admin" && $_GET["pass"] == "0000")
+		print('{"status":"PASSED","message":"Авторизация прошла успешно"}');
+	else
+		print('{"status":"FAILED","message":"Некорректный логин или пароль"}');
 }
-else{
-	print('{"status":"FAILED","message":"Некорректный логин или пароль"}');
+elseif (isset($_POST["post_name"]) && isset($_POST["post_pass"]))
+{
+	if ($_POST["post_name"] == "admin" && $_POST["post_pass"] == "0000")
+		print('{"status":"PASSED","message":"Авторизация прошла успешно"}');
+	else
+		print('{"status":"FAILED","message":"Некорректный логин или пароль"}');
+}
+else
+{
+	print('{"status":"ERROR","message":"Нет данных для авторизации"}');
 }
 
 ?>
