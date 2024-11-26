17d6cf27f99405436323f140d109884f81fb6083
Somov Studio <somov.studio@gmail.com>
2024-11-26 12:41:07 +0300

    Практика применения Unittest

---
+++ selenium/python/example-unittest/tests/TestAuthorization.py
@@ -0,0 +1,28 @@
+import unittest
+
+from selenium import  webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get('https://somovstudio.github.io/test_eng.html')
+        driver.find_element(By.NAME, 'login').send_keys('admin')
+        driver.find_element(By.NAME, 'pass').send_keys('0000')
+        driver.find_element(By.ID, 'buttonLogin').click()
+        element = driver.find_element(By.ID, 'result')
+        wait = WebDriverWait(driver, timeout=5)
+        wait.until(lambda d: element.is_displayed())
+        text = driver.find_element(By.ID, 'textarea').get_property('value')
+        print("Get message: " + text)
+        self.assertEqual(text, 'Authorization was successful')
+        driver.close()
+        driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
