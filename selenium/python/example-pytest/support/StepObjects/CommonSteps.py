6c03f83c44d50db598c9ea2692a98c98f34d3650
Somov Studio <somov.studio@gmail.com>
2024-12-02 13:53:36 +0300

    Создан автотест TestAuthorizationXUnit для Python

---
+++ selenium/python/example-pytest/support/StepObjects/CommonSteps.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+from support.PageObjects.CommonPage import CommonPage
+
+class CommonSteps:
+    driver = 0
+
+    def __init__(self, webdriver):
+        self.driver = webdriver
+
+    def sendForm(self, login, password):
+        self.driver.find_element(By.NAME, CommonPage.nameLogin).send_keys(login)
+        self.driver.find_element(By.NAME, CommonPage.namePassword).send_keys(password)
+        self.driver.find_element(By.ID, CommonPage.idButtonLogin).click()
---
+++ selenium/python/example-pytest/tests/TestAuthorizationXUnit.py
@@ -0,0 +1,23 @@
+import pytest
+
+from selenium import webdriver
+from support.PageObjects.CommonPage import CommonPage
+from support.StepObjects.CommonSteps import CommonSteps
+
+class TestAuthorizationXUnit:
+    driver = 0
+
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+    def test(self):
+        self.driver.get('https://somovstudio.github.io/test_eng.html')
+        tester = CommonSteps(self.driver)
+        CommonSteps.sendForm(tester, 'admin', '0000')
+        text = CommonPage.getResultText(self.driver)
+        assert text == 'Authorization was successful', "Получено некорректное сообщение"
+
+    def teardown_method(self):
+        self.driver.close()
+        self.driver.quit()
